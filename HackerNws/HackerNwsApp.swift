518010785518c33669b3b70304539884d789c2b3
EmanuelFeij <50448386+EmanuelFeij@users.noreply.github.com>
2023-06-25 15:26:14 +0100

    Initial Commit

---
+++ HackerNws/HackerNwsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackerNwsApp.swift
+//  HackerNws
+//
+//  Created by Emanuel Feijó on 25/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct HackerNwsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
